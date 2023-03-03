3e1759a7d8f485aacc900e25bbe87520d3f1dc58
michelu <ugo.michel01@etu.umontpellier.fr>
2023-03-03 20:55:50 +0100

    scroll animation

--- asset/js/main.js
+++ asset/js/main.js
@@ -45,3 +45,35 @@ function toggleSkills(){
 skillsHeader.forEach((el) =>{
     el.addEventListener('click', toggleSkills)
 })
+
+/*=== Scroll Reveal animation ===*/
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+/*Scroll home*/
+sr.reveal('.home_title',{})
+sr.reveal('.button',{delay: 200})
+sr.reveal('.home_img',{delay: 400})
+sr.reveal('.home_social-icon',{interval: 200})
+
+/*Scroll about*/
+sr.reveal('.about_img',{})
+sr.reveal('.about_subtitle',{delay: 200})
+sr.reveal('.about_text',{delay: 200})
+sr.reveal('.about_button',{delay: 400})
+
+/*Scroll skills*/
+sr.reveal('.skills_content',{interval: 200})
+
+/*Scroll skills*/
+sr.reveal('.work_img',{interval: 200})
+
+/*Scroll skills*/
+sr.reveal('.contact_imput',{interval: 200})
+
+
